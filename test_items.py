1cd200206b38618fabc931e1da0b1c361a260d2f
Мельникова <145825421+softsoftsofta@users.noreply.github.com>
2025-11-06 17:30:59 +0500

    test_items.py

--- test_items.py
+++ test_items.py
@@ -2,8 +2,8 @@ def test_guest_should_see_add_to_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     
-    # Проверяем наличие кнопки добавления в корзину
+    # проверка наличия кнопки добавления в корзину
     add_to_basket_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     
-    # Assert что кнопка есть на странице
+    # assert что кнопка есть на странице
     assert len(add_to_basket_button) > 0, "Add to basket button is not presented"
